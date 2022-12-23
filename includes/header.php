ba694acfbfe602b60adf239088740008977504d7
SagarDesai10 <108871994+SagarDesai10@users.noreply.github.com>
2022-12-23 07:01:49 -0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,63 @@
+<header>
+	
+	<div class="nav_toggle" onclick="toggle_class();">
+		
+		<span class="toggle_icon"></span>
+		
+	</div>
+	
+	<div onclick="remove_class()">
+	
+	<div class="main_nav">
+		
+		<h2>Apun KA adda</h2>
+		
+		<ul class="default_links">
+			
+			<li><a href="index.php">Home</a></li>
+			<li><a href="menu.php">Menu</a></li>
+			<li><a href="reservation.php">Reservation</a></li>
+			<li><a href="gallery.php">Gallery</a></li>
+			<li><a href="./admin/index.php">Admin</a></li>
+			
+			
+			
+		</ul>
+		
+		<p class="clear"></p>
+		
+	</div>
+	
+	<p class="clear"></p>
+	
+	</div>
+	
+</header>
+
+<div class="responive_nav">
+	
+	<div class="nav_section_img">
+		
+		<div class="nav_section_div">
+			
+			<h3>Apun KA adda</h3>
+			
+		</div>
+		
+	</div>
+	
+	<div class="nav_section">
+		
+		<ul>
+			
+			<li><a href="index.php"><span class="home">Home</span></a></li>
+			<li><a href="menu.php"><span class="menu">Menu</span></a></li>
+			<li><a href="reservation.php"><span class="reserve">Book Table</span></a></li>
+			
+			
+			
+		</ul>
+		
+	</div>
+	
+</div>
